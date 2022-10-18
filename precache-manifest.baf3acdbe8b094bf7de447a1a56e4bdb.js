cd41d4bf4fd047b8b5dc95cb02c7e38fd1f27c05
Your Name <debra4117@gmail.com>
2022-10-17 19:50:37 -0500

    Updates

---
+++ precache-manifest.baf3acdbe8b094bf7de447a1a56e4bdb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f829d572608d54caa8144cc27d75591",
+    "url": "/To-Do-List-React/index.html"
+  },
+  {
+    "revision": "ac275d26a31be32343d8",
+    "url": "/To-Do-List-React/static/js/2.99ce147d.chunk.js"
+  },
+  {
+    "revision": "7edcc0fdb26f7e8467b9",
+    "url": "/To-Do-List-React/static/js/main.38eac301.chunk.js"
+  },
+  {
+    "revision": "4c932a4a3e9fb27e87ab",
+    "url": "/To-Do-List-React/static/js/runtime-main.b185eec6.js"
+  }
+]);
